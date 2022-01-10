4fe704d79b2d81febeb552ae3ad63a0d1a59899c
Babahan-0906 <84456675+Babahan-0906@users.noreply.github.com>
2022-01-10 18:10:03 +0500

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -1,1 +1,1 @@
-$('body').backstretch(["https://drive.google.com/file/d/1PS5kXl8HLMH-4R2mo03RRmaoIuMhe4M2/view?usp=sharing"], {duration: 3200, fade: 1300});
+$('body').backstretch(["images/tm-bg-slide-1.jpg"], {duration: 3200, fade: 1300});
