fb461094dbb99361be1ade514d357075bffd8d40
Babahan-0906 <84456675+Babahan-0906@users.noreply.github.com>
2022-02-17 10:23:18 +0500

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -1,1 +1,1 @@
-$('body').backstretch(["https://drive.google.com/thumbnail?id=1iFJRT5FZsIoMcA0f2jba53xh2gRjj7Jk"], {duration: 3200, fade: 1300});
+$('body').backstretch(["images/pexels-felix-mittermeier-956981.jpg"], {duration: 3200, fade: 1300});
