c47c55761d352ce6c241174f6421b58d0070ae6c
Babahan-0906 <84456675+Babahan-0906@users.noreply.github.com>
2024-02-19 12:17:13 +0500

    Update custom.js
    Hiding background

--- js/custom.js
+++ js/custom.js
@@ -1,1 +1,1 @@
-$('body').backstretch(["images/pexels-felix-mittermeier-956981.jpg"], {duration: 3200, fade: 1300});
+$('body').backstretch(["images/pexels-felix-mittermeier-956981_.jpg"], {duration: 3200, fade: 1300});
